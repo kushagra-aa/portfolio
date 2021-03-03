c35ea0acb4adeca5fbeb79f0bce773440caf800e
kushagra-aa <kushagraaagnihotri@gmail.com>
2021-03-03 13:04:58 +0530

    made nav bar for mobile with hamburger

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector(".ham");
+// for the button
+const sideBar = document.querySelector(".side-bar");
+// for the ul list
+const links = document.querySelectorAll("nav ul li");
+// for the li items
+
+hamburger.addEventListener("click", () => {
+    sideBar.classList.toggle("open");
+    hamburger.classList.toggle("opened");
+    //   adds class open to navlinks
+    links.forEach(link => {
+        link.classList.toggle("fade");
+        //   adds class fade to links
+    });
+});
+// sideBar.addEventListener("click", () => {
+//     sideBar.classList.add("open");
+//     //   adds class open to navlinks
+//     links.forEach(link => {
+//         link.classList.toggle("fade");
+//         //   adds class fade to links
+//     });
+// });
